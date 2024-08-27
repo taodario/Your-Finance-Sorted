0f953d7330f06d74a899191bae323223e2cbacb4
DT <dariotaoasus@gmail.com>
2024-08-26 22:01:03 -0400

    Completed backend, takes a user uploaded pdf, parses it and displays json format in ejs views

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import multer from "multer";
 import path, { parse } from "path";
 import { parsePdf } from "./pdfParser.js";
+import axios from "axios";
 
 const port = 3000;
 const app = express();
@@ -12,11 +13,25 @@ app.get('/', (req, res) => {
 })
 
 app.post('/upload', upload.single('pdfFile'), async (req, res) => {
-    // res.send('File uploaded successfully');
-    
-    const pdfText = await parsePdf(req.file.path);
-    res.render('pdfText.ejs', {pdfText});
+    try {
+        const pdfText = await parsePdf(req.file.path);
 
+        // send POST to Flask API using axios
+        const response = await axios.post('http://localhost:5001/parse', pdfText, {
+            headers: {
+                'Content-Type': 'text/plain'
+            }
+        });
+
+        const data = response.data;
+        res.render('pdfText.ejs', { transactions: data});
+
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Internal Server Error')
+    }
+
+    // res.render('pdfText.ejs', {pdfText});
 })
 
 app.listen(port, () => {
